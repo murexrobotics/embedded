1a25e8beaa8d9dc4c191c874503758a5ec9741af
Altan Mehmet Ünver <77926625+L0ad1n6@users.noreply.github.com>
2023-11-27 14:04:04 -0500

    fix: changed code to work

--- anyesc-pwm/src/main.cpp
+++ anyesc-pwm/src/main.cpp
@@ -12,33 +12,25 @@ void setup() {
   pinMode(0, OUTPUT);
   pinMode(1, OUTPUT);
   pinMode(2, OUTPUT);
+  
   digitalWrite(0, HIGH);
+  digitalWrite(1, HIGH);
+  digitalWrite(2, HIGH);
 
   // Initialize UART
   Serial1.setRX(13);
   Serial1.setTX(12);
   Serial1.begin(115200);
-  analogWriteFreq(50);
   
-  // // Initialize thruster
+  // Initialize thruster
   servo.attach(PWM_PIN);
-  servo.writeMicroseconds(1500); // send "stop" signal to ESC.
-  delay(7000);
+
+  // MIN: 1000, MAX: 2000
+  servo.writeMicroseconds(1500);
+
+  // ... whatever you want next
 }
 
 void loop() {
-  if (Serial1.available() == 2) {
-    // Turn on LED to awknowlege that UART is working
-    digitalWrite(2, HIGH);
-
-    // Check if the message is for us
-    int address = Serial1.read();
-    Serial1.read();
-    if (address == ADDRESS || address == ALL_CALL_ADDRESS) {
-      // Blink LED to awknowlege that we're receiving data correctly
-      digitalWrite(1, HIGH);
-      delay(1000);
-      digitalWrite(1, LOW);
-    }
-  }
+  delay(100);
 }
