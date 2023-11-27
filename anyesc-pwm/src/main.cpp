0ff1c4527a3d94e0fe8adec5b9750a8d3ca6b575
Altan Mehmet Ünver <77926625+L0ad1n6@users.noreply.github.com>
2023-11-27 14:13:06 -0500

    fix: removed unnecessary configuration

--- anyesc-pwm/src/main.cpp
+++ anyesc-pwm/src/main.cpp
@@ -1,34 +1,23 @@
 #include <Servo.h>
 #include <Arduino.h>
 
-int PWM_PIN = 3;
+#define pwm_pin 3
+#define pulse_width 1500
 Servo servo;
 
-int ADDRESS = 54;
-int ALL_CALL_ADDRESS = 48;
-
 void setup() {
-  /// ANYESC MASCP CODE
+  // Turn on the LEDs
   pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  
   digitalWrite(0, HIGH);
+  pinMode(1, OUTPUT);
   digitalWrite(1, HIGH);
+  pinMode(2, OUTPUT);
   digitalWrite(2, HIGH);
-
-  // Initialize UART
-  Serial1.setRX(13);
-  Serial1.setTX(12);
-  Serial1.begin(115200);
   
+
   // Initialize thruster
-  servo.attach(PWM_PIN);
-
-  // MIN: 1000, MAX: 2000
-  servo.writeMicroseconds(1500);
-
-  // ... whatever you want next
+  servo.attach(pwm_pin);
+  servo.writeMicroseconds(pulse_width);
 }
 
 void loop() {
