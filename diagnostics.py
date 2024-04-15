35b53a552f153490e2406e393f18fc1b1bf9de50
Altan Mehmet Ünver <77926625+L0ad1n6@users.noreply.github.com>
2024-04-15 18:40:42 -0400

    feat[diagnostics]: Fixed camera + generalized some IP's + changed .format() to f strings

--- diagnostics.py
+++ diagnostics.py
@@ -6,47 +6,46 @@ import time
 import atexit
 from colored import Fore, Back, Style
 
-def exit_handler():
-    if args.init or args.ardupilot:
-        os.system("{} 'echo {} | sudo --stdin killall ardusub'".format(prefix, system_password))
-        print("\n\nEnded all Ardusub processes")
+# Check to see if Byran is the user
+user = os.popen("whoami").read().strip()
+if user == "byran":
+    print("Your robot authorization privileges have been revoked.")
+    exit()
 
-atexit.register(exit_handler)
-
-self_ip = os.popen("ifconfig | grep 'inet ' | grep -v 127.0.0.1 | cut -d\\  -f2 | grep '192'").read()
+self_ip = os.popen("ifconfig | grep 'inet ' | grep -v 127.0.0.1 | cut -d\\  -f2 | grep '192'").read().strip()
 
 username = "murex"
 path_string = "/usr/local/bin:/usr/bin:/bin:/usr/games"
 system_password = "murex"
 ip_addr = "192.168.8.161"
 
-prefix = "sshpass -p {} ssh {}@{}".format(system_password, username, ip_addr)
-
+prefix = f"sshpass -p {system_password} ssh {username}@{ip_addr}"
 parser = argparse.ArgumentParser(description='MUREX Robotics diagnostics utility')
-parser.add_argument('-ns', '--networkscan',
-                    action='store_true')  # on/off flag
-parser.add_argument('-i2c', '--i2cscan',
-                    action='store_true')  # on/off flag
-parser.add_argument('-ping', '--ping',
-                    action='store_true')  # on/off flag
-parser.add_argument('-mascp', '--mascp',
-                    action='store_true')  # on/off flag
-parser.add_argument('-ap', '--ardupilot',
-                    action='store_true')  # on/off flag
-parser.add_argument('-usb3', '--usb3',
-                    action='store_true')  # on/off flag
-parser.add_argument('-usb', '--usb',
-                    action='store_true')  # on/off flag
-parser.add_argument('-ffmpeg', '--ffmpeg',
-                    action='store_true')  # on/off flag
-parser.add_argument('-r', '--reboot',
-                    action='store_true')  # on/off flag
-parser.add_argument('-p', '--power',
-                    action='store_true')  # on/off flag
-parser.add_argument('-init', '--init',
-                    action='store_true')  # on/off flag
+
+parser.add_argument('-ns', '--networkscan', action='store_true')  # on/off flag
+parser.add_argument('-i2c', '--i2cscan', action='store_true')  # on/off flag
+parser.add_argument('-ping', '--ping', action='store_true')  # on/off flag
+parser.add_argument('-mascp', '--mascp', action='store_true')  # on/off flag
+parser.add_argument('-ap', '--ardupilot', action='store_true')  # on/off flag
+parser.add_argument('-usb3', '--usb3', action='store_true')  # on/off flag
+parser.add_argument('-usb', '--usb', action='store_true')  # on/off flag
+parser.add_argument('-v1', '--video1', action='store_true')  # on/off flag
+parser.add_argument('-v2', '--video2', action='store_true')  # on/off flag
+parser.add_argument('-r', '--reboot', action='store_true')  # on/off flag
+parser.add_argument('-p', '--power', action='store_true')  # on/off flag
+parser.add_argument('-init', '--init', action='store_true')  # on/off flag
+parser.add_argument('-a', '--all', action='store_true')  # on/off flag
+parser.add_argument('-ip', '--customip', type=str, action='store')  # on/off flag
 
 args = parser.parse_args()
+
+def exit_handler():
+    if args.init or args.ardupilot:
+        os.system(f"{prefix} 'echo {system_password} | sudo --stdin killall ardusub'")
+        os.system(f"{prefix} 'echo {system_password} | sudo --stdin killall ffmpeg'")
+        print("\n\nEnded all Ardusub processes")
+
+atexit.register(exit_handler)
 
 print(f'{Style.BOLD}{Fore.white}{Back.black}\n\nMUREX ROBOTICS.\n\tATTEMPT THE IMPOSSIBLE.\n{Style.reset}')
 
@@ -54,12 +53,12 @@ print("Local IP Address: {}".format(self_ip))
 
 if args.init:
     print(f'{Style.BOLD}{Fore.white}\n\nRunning NMAP\n\n{Style.reset}')
-    os.system("sudo --stdin nmap -sn -n -T5 192.168.8.0/24 | grep -e 192 -e MAC")
+    os.system(f"sudo --stdin nmap -sn -n -T5 {".".join(ip_addr.strip().split(".")[0:3])}.0/24 | grep -e 192 -e MAC")
 
     time.sleep(1)
 
     print(f'{Style.BOLD}{Fore.white}\n\nRunning I2C Scan\n\n{Style.reset}')
-    i2c_output = os.popen("{} 'echo {} | sudo --stdin i2cdetect -y 1'".format(prefix, system_password)).read()
+    i2c_output = os.popen(f"{prefix} 'echo {system_password} | sudo --stdin i2cdetect -y 1'").read()
     print(i2c_output)
     if "77" in i2c_output:
         print(f'{Style.BOLD}{Fore.green}BME680 at 0x77 found!\n{Style.reset}')
@@ -89,55 +88,66 @@ if args.init:
     time.sleep(1)
 
     print(f'{Style.BOLD}{Fore.white}\n\nGetting INA226 Power Monitoring Data\n\n{Style.reset}')
-    os.system("{} 'echo {} | sudo --stdin python3 ~/pi_ina226/example.py'".format(prefix, system_password))
+    os.system(f"{prefix} 'echo {system_password} | sudo --stdin python3 ~/pi_ina226/example.py'")
 
     time.sleep(1)
 
     print(f'{Style.BOLD}{Fore.white}\n\nRunning MASCP Binary\n\n{Style.reset}')
-    os.system("{} 'echo {} | sudo --stdin ~/mascp'".format(prefix, system_password))
+    os.system(f"{prefix} 'echo {system_password} | sudo --stdin ~/mascp'")
 
     time.sleep(1)
 
     print(f'{Style.BOLD}{Fore.white}\n\nInitializing uPD720202\n\n{Style.reset}')
-    os.system("{} 'echo {} | sudo --stdin ~/upd72020x-load/upd72020x-check-and-init' ".format(prefix, system_password))
-    os.system("{} 'lsusb' ".format(prefix))
+    os.system(f"{prefix} 'echo {system_password} | sudo --stdin ~/upd72020x-load/upd72020x-check-and-init' ")
+    os.system(f"{prefix} 'lsusb' ")
 
     time.sleep(1)
 
     print(f'{Style.BOLD}{Fore.white}\n\nRunning Ardusub Binary\n\n{Style.reset}')
     os.system("open -a QGroundControl")
-    os.system("{} 'echo {} | sudo --stdin ~/mrx/usr/bin/ardusub --serial0 tcp:192.168.8.161:5760 --serial1 /dev/ttyAMA4' ".format(prefix, system_password))
+    os.system(f"{prefix} 'echo {system_password} | sudo --stdin ~/mrx/usr/bin/ardusub --serial0 tcp:{ip_addr}:5760 --serial1 /dev/ttyAMA4' &")
+    os.system("ping {}".format(ip_addr))
 
-if args.ffmpeg:
+if args.customip:
+    print(".".join(ip_addr.strip().split(".")[0:3]))
+    ip_addr = args.customip
+
+if args.video1:
     print(f'{Style.BOLD}{Fore.white}\n\nRunning FFmepg on both cameras\n\n{Style.reset}')
-    os.system("ffplay -fflags nobuffer -flags low_delay -framedrop -vf vflip -probesize 32 -strict experimental udp://{}:5602".format(ip_addr))
-    os.system("{} 'echo {} | sudo --stdin ffmpeg -f v4l2 -i /dev/video0 -c:v h264_v4l2m2m -vf scale=1920x1080 -b:v 3000k -fflags nobuffer -flags low_delay -preset ultrafast -tune zerolatency -probesize 32 -num_output_buffers 32 -num_capture_buffers 16 -analyzeduration 0 -f mpegts udp://{}:5602' ".format(prefix, system_password, self_ip))
+    os.system(f"{prefix} 'ffmpeg -f v4l2 -i /dev/video0 -c:v h264_v4l2m2m -vf \"hflip,format=yuv420p,scale=1920x1080\" -b:v 9000k -fflags nobuffer -flags low_delay -preset ultrafast -tune zerolatency -probesize 32 -num_output_buffers 32 -num_capture_buffers 16 -analyzeduration 0 -f mpegts udp://{self_ip}:5600'")
+    os.system(f"ffplay -fflags nobuffer -flags low_delay -framedrop -vf vflip -probesize 32 -strict experimental udp://{ip_addr}:5600")
 
+if args.video2:
+    print(f'{Style.BOLD}{Fore.white}\n\nRunning FFmepg on both cameras\n\n{Style.reset}')
+    os.system(f"{prefix} 'ffmpeg -f v4l2 -i /dev/video2 -c:v h264_v4l2m2m -vf \"hflip,format=yuv420p,scale=720x1280\" -b:v 9000k -fflags nobuffer -flags low_delay -preset ultrafast -tune zerolatency -probesize 32 -num_output_buffers 32 -num_capture_buffers 16 -analyzeduration 0 -f mpegts udp://{self_ip}:5601'")
+    os.system(f"ffplay -fflags nobuffer -flags low_delay -framedrop -vf vflip -probesize 32 -strict experimental udp://{ip_addr}:5601")
+    
 if args.ping:
     print(f'{Style.BOLD}{Fore.white}\n\nPinging MUREX Carrier Board\n\n{Style.reset}')
-    os.system("ping {}".format(ip_addr))
+    os.system(f"ping {ip_addr}")
 
 if args.power:
     print(f'{Style.BOLD}{Fore.white}\n\nGetting INA226 Power Monitoring Data\n\n{Style.reset}')
-    os.system("{} 'echo {} | sudo --stdin python3 ~/pi_ina226/example.py'".format(prefix, system_password))
+    os.system(f"{prefix} 'echo {system_password} | sudo --stdin python3 ~/pi_ina226/example.py'")
 
 if args.networkscan:
     print(f'{Style.BOLD}{Fore.white}\n\nRunning NMAP\n\n{Style.reset}')
-    os.system("sudo --stdin nmap -sn -n -T5 192.168.8.0/24 | grep -e 192 -e MAC")
+    os.system(f"sudo --stdin nmap -sn -n -T5 {".".join(ip_addr.strip().split(".")[0:3])}.0/24 | grep -e 192 -e MAC")
+
 
 if args.mascp:
     print(f'{Style.BOLD}{Fore.white}\n\nRunning MASCP Binary\n\n{Style.reset}')
-    os.system("{} 'echo {} | sudo --stdin ~/mascp'".format(prefix, system_password))
+    os.system(f"{prefix} 'echo {system_password} | sudo --stdin ~/mascp'")
 
 if args.reboot:
     print(f'{Style.BOLD}{Fore.white}\n\nRebooting...\n\n{Style.reset}')
-    os.system("{} 'echo {} | sudo --stdin reboot'".format(prefix, system_password))
+    os.system(f"{prefix} 'echo {system_password} | sudo --stdin reboot'")
     time.sleep(7)
     os.system("ping {}".format(ip_addr))
 
 if args.i2cscan:
     print(f'{Style.BOLD}{Fore.white}\n\nRunning I2C Scan\n\n{Style.reset}')
-    i2c_output = os.popen("{} 'echo {} | sudo --stdin i2cdetect -y 1'".format(prefix, system_password)).read()
+    i2c_output = os.popen(f"{prefix} 'echo {system_password} | sudo --stdin i2cdetect -y 1'").read()
     if "77" in i2c_output:
         print(f'{Style.BOLD}{Fore.green}BME680 at 0x77 found!\n{Style.reset}')
     else:
@@ -166,13 +176,13 @@ if args.i2cscan:
 if args.ardupilot:
     print(f'{Style.BOLD}{Fore.white}\n\nRunning Ardusub Binary\n\n{Style.reset}')
     os.system("open -a QGroundControl")
-    os.system("{} 'echo {} | sudo --stdin ~/mrx/usr/bin/ardusub --serial0 tcp:192.168.8.161:5760 --serial1 /dev/ttyAMA4' ".format(prefix, system_password))
+    os.system(f"{prefix} 'echo {system_password} | sudo --stdin ~/mrx/usr/bin/ardusub --serial0 tcp:{ip_addr}:5760 --serial1 /dev/ttyAMA4' ")
 
 if args.usb3:
     print(f'{Style.BOLD}{Fore.white}\n\nInitializing uPD720202\n\n{Style.reset}')
-    os.system("{} 'echo {} | sudo --stdin ~/upd72020x-load/upd72020x-check-and-init' ".format(prefix, system_password))
-    os.system("{} 'lsusb' ".format(prefix))
+    os.system(f"{prefix} 'echo {system_password} | sudo --stdin ~/upd72020x-load/upd72020x-check-and-init' ")
+    os.system(f"{prefix} 'lsusb' ")
 
 if args.usb:
     print(f'{Style.BOLD}{Fore.white}\n\nRunning lsusb\n\n{Style.reset}')
-    os.system("{} 'lsusb' ".format(prefix))
+    os.system(f"{prefix} 'lsusb' ")
